d6acd7bf73708a6f9edd29c1508c613329812cc6
Dylan <ventdylan@gmail.com>
2021-10-11 22:48:49 -0400

    #2 second check of entries (valid form when user clicks submit) #3 modification of specific error messages

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,8 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-
-// the element with the .close class has been recovered
+const sendButtonForm = document.querySelector(".btn-submit");
+const modalBody = document.querySelector(".modal-body");
 const closeTheModal = document.querySelector(".close"); 
 
 //creation of the span element
@@ -29,6 +29,10 @@ const errorMessageEmail = document.createElement("span");
 errorMessageEmail.classList.add("error-message");
 formData[2].appendChild(errorMessageEmail);
 
+const errorMessageBirthdateField = document.createElement("span");
+errorMessageBirthdateField.classList.add("error-message");
+formData[3].appendChild(errorMessageBirthdateField);
+
 
 const errorMessageCompetition = document.createElement("span");
 errorMessageCompetition.classList.add("error-message");
@@ -43,216 +47,121 @@ const errorMessagesConditionsOfUse = document.createElement("span");
 errorMessagesConditionsOfUse.classList.add("error-message");
 formData[6].appendChild(errorMessagesConditionsOfUse);
 
-// verification of the field (first name)
-function firstNameVerification() {
-  const firstName = document.querySelector("#firstname").value;
-  // console.log(firstName.length);
-  if(firstName.length < 1) {
-  errorMessageFirstNameField.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom";
+function verification() {
+  const firstName = document.querySelector("#firstname").value.length;
+  const lastName = document.querySelector("#lastname").value.length;
+  const email = document.querySelector("#email").value;
+  const emailPattern =  /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  const birthDate = new Date(document.querySelector("#birthdate").value);
+  const date = birthDate.getDate();
+  const month = birthDate.getMonth();  
+  const year = birthDate.getFullYear();  
+  const tournaments = document.querySelector("#tournaments").value;
+  const locationNewYork = document.querySelector("#location1").checked;
+  const locationSanFrancisco = document.querySelector("#location2").checked;
+  const locationSeattle = document.querySelector("#location3").checked;
+  const locationChicago = document.querySelector("#location4").checked;
+  const locationBoston = document.querySelector("#location5").checked;
+  const locationPortland = document.querySelector("#location6").checked;
+  const conditionsOfUse = document.querySelector("#conditionsofuse").checked;
+  const nextEvent = document.querySelector("#nextevent").checked;
+  
+  console.log(firstName);
+  console.log(lastName);
+  console.log(email);
+
+  console.log(date);
+  console.log(month);
+  console.log(year);
+  console.log(birthDate);
+  console.log(tournaments);
+
+  console.log(locationNewYork);
+  console.log(locationSanFrancisco);
+  console.log(locationSeattle);
+  console.log(locationChicago);
+  console.log(locationBoston);
+  console.log(locationPortland);
+
+  console.log(conditionsOfUse);
+  console.log(nextEvent);
+
+  //verification of the field first name
+
+  if(firstName < 2 || firstName == "") {
+    errorMessageFirstNameField.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom";
     // console.log("Veuillez entrer 2 caractères ou plus pour le champ du prénom");
-  } else if (firstName.length >= 12) {
+  } else if (firstName >= 13) {
     errorMessageFirstNameField.textContent = "Veuillez entrer moins de caractères pour le champ du prénom";
     // console.log("Veuillez entrer moins de caractères pour le champ du prénom");
   } else {
     errorMessageFirstNameField.textContent = "";
   }
-}
+  
+  //verification of the field last name
 
-document.querySelector("#firstname").addEventListener("keydown",firstNameVerification);
-
-// verification of the field (last name)
-
-function lastNameVerification() {
-  const lastName = document.querySelector("#lastname").value;
-  // console.log(lastName.length);
-  if(lastName.length < 1) {
-    // console.log("Veuillez entrer 2 caractères ou plus pour le champ du nom"); 
+  if(lastName < 2 || lastName == "") {
     errorMessageLastNameField.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du nom";  //error message first name field
-  } else if (lastName.length >= 12) {
+    // console.log("Veuillez entrer 2 caractères ou plus pour le champ du nom"); 
+  } else if (lastName >= 13) {
     errorMessageLastNameField.textContent = "Veuillez entrer moins de caractères pour le champ du nom";
     // console.log("Veuillez entrer moins de caractères pour le champ du prénom");
   } else {
     errorMessageLastNameField.textContent = "";
   }
-}
 
-document.querySelector("#lastname").addEventListener("keydown",lastNameVerification);
+  //email field verification
 
-//verification of the field (email)
-
-function verificationEmail() {
-  const email = document.querySelector("#email").value;
-  //the regular expression allows to check the email 
-  const emailPattern =  /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-  // console.log(email);
-  if(emailPattern.test(email) == true) {
-    // console.log("Votre email est valide");
-    errorMessageEmail.textContent = "";
-  } else {
-    // console.log("Veuillez saisir une adresse email valide Exp(lorem@gmail.com)");
+  if(emailPattern.test(email) == false) {
     errorMessageEmail.textContent = "Veuillez saisir une adresse email valide Exp(lorem@gmail.com)";
+    // console.log("Veuillez saisir une adresse email valide Exp(lorem@gmail.com)");
+  } else {
+    errorMessageEmail.textContent = "";
+    // console.log("Votre email est valide");
   }
-  return emailPattern.test(email);
-}
 
-document.querySelector("#email").addEventListener("keydown",verificationEmail);
+  //verification of the date of birth field
 
-//verification entered (competetion)
+  if(date && month && year) {
+    errorMessageBirthdateField.textContent = "";
+    // console.log("Date de naissance saisie");
+  } else {
+    errorMessageBirthdateField.textContent = "veuillez entrer votre date de naissance"
+    // console.log("veuillez entrer votre date de naissance");
+  }
 
-function entryCompetition() { 
-  const numberOfTournaments = document.querySelector("#quantity").value;
-  // console.log(numberOfTournaments);
-  if(Number(numberOfTournaments) >= 0 && Number(numberOfTournaments) <= 99) {
-    // console.log("Une valeur a bien été saisie");
+  //verification of the tournament participation field
+
+  if(tournaments >= 0 && tournaments <= 99) {
     errorMessageCompetition.textContent = "";
+    // console.log("Une valeur a bien été saisie");
   } else {
     errorMessageCompetition.textContent = "Veuillez entrer une valeur entre 0 et 99";
   }
-}
 
-document.querySelector("#quantity").addEventListener("keydown",entryCompetition);
+  //verification of the location buttons
 
-//button check (location)
-
-function buttonRadioLocationNewyork() {
-  const locationNewyork = document.querySelector("#location1").checked;
-
-  console.log(locationNewyork);
-  if(locationNewyork === true) {
-    // console.log("La ville a bien été choisie");
+  if(locationNewYork || locationSanFrancisco || locationSeattle || locationChicago || locationBoston || locationPortland == true ) {
     buttonErrorMessage.textContent = "";
-  }  else {
-    // console.log("Veuillez sélectionner une ville");
-    buttonErrorMessage.textContent = "Veuillez sélectionner une ville";
-  }
-}
-
-document.querySelector("#location1").addEventListener("click",buttonRadioLocationNewyork);
-
-function buttonRadioLocationSanFrancisco() {
-  const locationSanFrancisco = document.querySelector("#location2").checked;
-  
-  console.log(locationSanFrancisco);
-  if(locationSanFrancisco === true) {
     // console.log("La ville a bien été choisie");
-    buttonErrorMessage.textContent = "";
   }  else {
     // console.log("Veuillez sélectionner une ville");
     buttonErrorMessage.textContent = "Veuillez sélectionner une ville";
   }
-}
 
-document.querySelector("#location2").addEventListener("click",buttonRadioLocationSanFrancisco);
+  // verification of the conditions of use
 
-function buttonRadioLocationSeattle() {
-  const locationSeattle = document.querySelector("#location3").checked;
-  console.log(locationSeattle);
-  
-  if(locationSeattle === true) {
-   // console.log("La ville a bien été choisie");
-   buttonErrorMessage.textContent = "";
-  }  else {
-    // console.log("Veuillez sélectionner une ville");
-    buttonErrorMessage.textContent = "Veuillez sélectionner une ville";
-  }
-}
-
-document.querySelector("#location3").addEventListener("click",buttonRadioLocationSeattle);
-
-function buttonRadioLocationChicago() {
-  const locationChicago = document.querySelector("#location4").checked;
-  console.log(locationChicago);
-
-  if(locationChicago === true) {
-   // console.log("La ville a bien été choisie");
-   buttonErrorMessage.textContent = "";
-  }  else {
-    // console.log("Veuillez sélectionner une ville");
-    buttonErrorMessage.textContent = "Veuillez sélectionner une ville";
-  }
-}
-
-document.querySelector("#location4").addEventListener("click",buttonRadioLocationChicago);
-
-function buttonRadioLocationBoston() {
-  const locationBoston = document.querySelector("#location5").checked;
-  console.log(locationBoston);
-  
-  if(locationBoston === true) {
-   // console.log("La ville a bien été choisie");
-   buttonErrorMessage.textContent = "";
-  }  else {
-    // console.log("Veuillez sélectionner une ville");
-    buttonErrorMessage.textContent = "Veuillez sélectionner une ville";
-  }
-}
-
-document.querySelector("#location5").addEventListener("click",buttonRadioLocationBoston);
-
-function buttonRadioLocationPortland() {
-  const locationPortland = document.querySelector("#location6").checked;
-  console.log(locationPortland);
-
-  if(locationPortland === true) {
-  // console.log("La ville a bien été choisie");
-  buttonErrorMessage.textContent = "";
-}  else {
-  // console.log("Veuillez sélectionner une ville");
-  buttonErrorMessage.textContent = "Veuillez sélectionner une ville";
-}
-}
-
-document.querySelector("#location6").addEventListener("click",buttonRadioLocationPortland);
-
-// verification of terms and conditions of use
-function termsAndConditions() {
-  const general_Conditions = document.querySelector("#conditions").checked;
-  // console.log(general_Conditions);
-
-  if(general_Conditions === true) {
-    // console.log("Les conditions générales sont acceptées");
+  if(conditionsOfUse == true) {
     errorMessagesConditionsOfUse.textContent = "";
+    // console.log("Les conditions générales ont été acceptées");
   } else {
-    // console.log("Veuillez cocher la case des conditions d'utilisation");
     errorMessagesConditionsOfUse.textContent = "Veuillez cocher la case des conditions d'utilisation";
+    // console.log("Veuillez cocher la case des conditions d'utilisation");
   }
 }
 
-document.querySelector("#conditions").addEventListener("click",termsAndConditions);
-
-// newsletter
-function newsLetter() {
-  const newsletter = document.querySelector("#newsletter").checked;
-  // console.log(newsletter);
-
-  if(newsletter === true) {
-    console.log("vous souhaitez être prévenus des prochains évènements");
-  } else {
-    console.log("vous ne souhaitez pas être prévenus des prochains évènements");
-  }
-}
-
-document.querySelector("#newsletter").addEventListener("click",newsLetter);
-
-// function dateBirthday() {
-//   const dateEntered = new Date(document.querySelector("#birthdate").value);
-//   console.log(dateEntered);
-//     const day = dateEntered.getDay();
-//     const month = dateEntered.getMonth();
-//     const year = dateEntered.getUTCFullYear();
-//     console.log(day);
-//     console.log(month);
-//     console.log(year);
-// }
-
-document.querySelector("#birthdate").addEventListener("keydown",dateBirthday);
-
-
-
-
-
-
-
+//addeventlistener method used on the submit button
+sendButtonForm.addEventListener("click",verification);
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -262,7 +171,7 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-//close the form
+//close the modal
 closeTheModal.addEventListener("click", closeTheModalForm); 
 
 function closeTheModalForm() {
