75f6e83ed5bdeb86580334544645781c7e977497
Dylan <ventdylan@gmail.com>
2021-10-13 23:00:53 -0400

    #4 confirmation message added

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -65,26 +65,28 @@ function verification() {
   const locationPortland = document.querySelector("#location6").checked;
   const conditionsOfUse = document.querySelector("#conditionsofuse").checked;
   const nextEvent = document.querySelector("#nextevent").checked;
+
+  // verification of values in the console
   
-  console.log(firstName);
-  console.log(lastName);
-  console.log(email);
+  // console.log(firstName);
+  // console.log(lastName);
+  // console.log(email);
 
-  console.log(date);
-  console.log(month);
-  console.log(year);
-  console.log(birthDate);
-  console.log(tournaments);
+  // console.log(date);
+  // console.log(month);
+  // console.log(year);
+  // console.log(birthDate);
+  // console.log(tournaments);
 
-  console.log(locationNewYork);
-  console.log(locationSanFrancisco);
-  console.log(locationSeattle);
-  console.log(locationChicago);
-  console.log(locationBoston);
-  console.log(locationPortland);
+  // console.log(locationNewYork);
+  // console.log(locationSanFrancisco);
+  // console.log(locationSeattle);
+  // console.log(locationChicago);
+  // console.log(locationBoston);
+  // console.log(locationPortland);
 
-  console.log(conditionsOfUse);
-  console.log(nextEvent);
+  // console.log(conditionsOfUse);
+  // console.log(nextEvent);
 
   //verification of the field first name
 
@@ -136,7 +138,7 @@ function verification() {
     errorMessageCompetition.textContent = "";
     // console.log("Une valeur a bien été saisie");
   } else {
-    errorMessageCompetition.textContent = "Veuillez entrer une valeur entre 0 et 99";
+    errorMessageCompetition.textContent = "Veuillez entrer un nombre entre 0 et 99";
   }
 
   //verification of the location buttons
@@ -158,6 +160,19 @@ function verification() {
     errorMessagesConditionsOfUse.textContent = "Veuillez cocher la case des conditions d'utilisation";
     // console.log("Veuillez cocher la case des conditions d'utilisation");
   }
+}  
+
+// successful submission confirmation message for the user
+
+confirmationMessage();
+
+function confirmationMessage() {
+  const messageContainer = document.createElement("div");
+  messageContainer.classList.add("message-container");
+  messageContainer.innerHTML = 
+  "<div class='message-content'><span class='message-close'></span><div class='message-body'><p class='message'>Vos coordonnées ont été transmises avec succès.<br> Et votre place a bien été réserver</p><button class='btn-close2'>Fermer</button></div></div>";
+  messageContainer.style.display = "flex";
+  document.querySelector("main").appendChild(messageContainer);
 }
 
 //addeventlistener method used on the submit button
@@ -176,4 +191,24 @@ closeTheModal.addEventListener("click", closeTheModalForm);
 
 function closeTheModalForm() {
   modalbg.style.display="none";
+}
+
+// closing button modal 2
+
+const btnClose = document.querySelector(".message-close");
+const btnClose2 = document.querySelector(".btn-close2");
+
+// modal two
+const modalTwo = document.querySelector(".message-container");
+
+btnClose.addEventListener("click", closeButton);
+
+function closeButton() {
+  modalTwo.style.display = "none";
+}
+
+btnClose2.addEventListener("click",closeButton2);
+
+function closeButton2() {
+  modalTwo.style.display = "none";
 }
